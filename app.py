a89f0b125700346fac0940f6fb54ea281336a61a
Robin Das <dassrobin77@gmail.com>
2021-09-30 13:39:57 +0530

    Updated CSV data not reflecting

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import flask
 from flask import request,render_template
 import pandas as pd
 from flask_cors import CORS, cross_origin
+import os
 
-cols=["pg","word","definition","sentence","category","sample","synonyms"]
-data = pd.read_csv('ISLT_data.csv', sep=',', engine='python', usecols=cols,na_values = [''])
-json_data = data.to_json(orient = "records")
-json_list = json_data.replace("\\","")
-print(json_list)
+csv_path = "./ISLT_data.csv" # file should be saved somewhere else, should not be a part of build folder
+
+def retrieveData():
+    cols=["pg","word","definition","sentence","category","sample","synonyms"]
+    data = pd.read_csv(csv_path, sep=',', usecols=cols,na_values = [''])
+    json_data = data.to_json(orient = "records")
+    json_list = json_data.replace("\\","")
+    print(json_list)
+    return json_list
 
 
 app = flask.Flask(__name__,template_folder='template')
@@ -18,15 +23,20 @@ print('***Backend Running***')
 
 @app.route('/')
 def home():
-   return render_template('create.html')
+    return render_template('create.html')
+
 @app.route('/getData',methods=['GET'])
 def getData():
-    return json_list
-@app.route('/sendData', methods=['POST']) 
+    return retrieveData()
+
+@app.route('/sendData', methods=['GET','POST']) 
 @cross_origin()
 def sendData():
-    postData= pd.json_normalize(request.form)
-    newData = pd.concat([data,postData],ignore_index=True)
-    # adding new data into csv
-    newData.to_csv('ISLT_data.csv',index=False)
+    if request.method == "GET":
+        postData= pd.json_normalize(request.form)
+        newData = pd.concat([data,postData],ignore_index=True)
+        # adding new data into csv
+        os.remove(csv_path)
+        newData.to_csv(csv_path,index=False)
+        print(postData)
     return request.form
